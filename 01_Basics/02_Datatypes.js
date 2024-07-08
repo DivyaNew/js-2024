e535187489a3e9d7f35d6588ed63abc71461e18d
Divya <157968530+DivyaNew@users.noreply.github.com>
2024-07-08 16:29:44 +0000

    datatypes summary

--- 01_Basics/02_Datatypes.js
+++ 01_Basics/02_Datatypes.js
@@ -6,6 +6,8 @@ let name = "Divya"
 let age =100
 let loggedIn = true;
 
+//primitive - 7types
+
 //number => range - 2 to power 53
 //bigint
 //sting => "";
@@ -14,8 +16,27 @@ let loggedIn = true;
 //undefined => value is not yet assigned
 //symbol => unique
 
-console.log(typeof null); //object
-console.log(typeof undefined); //undefined
+// console.log(typeof null); //object
+// console.log(typeof undefined); //undefined
 
-//object 
+// reference (non primitive)
+//object , arrays, functions,
+
+const id = Symbol('12');
+const anotherid = Symbol('12');
+
+//console.log(id === anotherid); // false
+//console.log(typeof id); //symbol
+
+const bigNumber = 36746252728637n;
+
+console.log(typeof bigNumber); // bigInt
+
+const myFunction = {
+    name: 'Divya',
+};
+
+console.log(typeof myFunction); // object /function-object
+
+
 

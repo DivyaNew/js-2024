8c1593058d1486991719f926dab08eddff8987bf
Divya <157968530+DivyaNew@users.noreply.github.com>
2024-07-12 04:04:15 +0000

    Objects...part2

--- 01_Basics/09_Objects.js
+++ 01_Basics/09_Objects.js
@@ -53,3 +53,76 @@ JsUser.greetingTwo = function (){
 console.log(JsUser.greeting); //[Function (anonymous)]
 console.log(JsUser.greeting()); //Hello JS User!
 console.log(JsUser.greetingTwo()); //Hello JS User, Divya H
+
+//part 2
+//const instaUser = new Object(); //singleton
+
+const instaUser ={} //non-singleton
+//console.log(instaUser);//{}
+
+instaUser.id="Sam123";
+instaUser.name="Sam";
+instaUser.isLoggedIn = true;
+//console.log(instaUser);//{id="Sam123",name="Sam",isLoggedIn = true}
+
+const regularUser = {
+    email:"reg@gmail.com",
+    fullname:{
+        userfullname :{
+            fname:"Divya",
+            lname:"H"
+        }
+    }
+}
+
+// console.log(regularUser);
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname.fname);
+
+const obj1 ={ 1:"a", 2:"b"};
+const obj2 ={ 3:"a", 4:"b"};
+
+//const obj3 = {obj1,obj2};
+//console.log(obj3);
+
+//const obj3 = Object.assign({}, obj1,obj2); //assign({target,source})
+//console.log(obj3);//{1:"a", 2:"b",3:"a", 4:"b"}
+
+const obj3 = {...obj1,...obj2};
+//console.log(obj3);
+
+console.log(Object.keys(instaUser));
+console.log(Object.values(instaUser));
+console.log(Object.entries(instaUser));
+
+console.log(instaUser.hasOwnProperty('isLoggedIn')); //true
+console.log(instaUser.hasOwnProperty('isLogged')); //false
+
+
+//destructuring
+
+// const course ={
+//     cname:"Javascript",
+//     price:999,
+//     courseInstructor: "Hitesh"
+// }
+
+// const {courseInstructor} = course;
+// console.log(courseInstructor);
+
+// const {courseInstructor : CI} = course;
+// console.log(CI);
+
+//JSON format
+
+// {
+//     "cname":"JS",
+//     "fees":"free"
+//     "courseInstructor": "HC"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]

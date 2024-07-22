59342b77a1003315dfa07e74fabe5954f88f6d14
Divya <157968530+DivyaNew@users.noreply.github.com>
2024-07-22 12:22:28 +0000

    fun part2

--- 01_Basics/10_Functions.js
+++ 01_Basics/10_Functions.js
@@ -26,3 +26,41 @@ function addNumbers (n1,n2) { //n1,n2 - parameters
  //console.log(userLoginMsg("Divya"));// Divya just loggen in
  //console.log(userLoginMsg("")); //just loggen in
  console.log(userLoginMsg()); //undefined just loggen in 
+
+ function calculateCartPrice (...num1){
+    return num1;
+}
+
+console.log(calculateCartPrice(200,300,400)); //[200,300,400]
+
+function calculateCartPrice1 (val1,val2,...num1){
+return num1;
+}
+
+console.log(calculateCartPrice1(200,300,400,2000)); //[400,2000]
+
+const user ={
+name:"Divya",
+Price:1999
+}
+
+function handleObject(anyObject){
+console.log(`username is ${anyObject.name} and price is ${anyObject.Price}`);
+}
+
+handleObject(user);
+
+handleObject({
+name:"Random",
+Price:1000
+})
+
+
+const myNewArray = [100,2000,500,1000];
+
+function returnSecValue(getArray){
+return getArray[1];
+}
+
+console.log(returnSecValue(myNewArray));
+console.log(returnSecValue([1000,200,2000,400]));
